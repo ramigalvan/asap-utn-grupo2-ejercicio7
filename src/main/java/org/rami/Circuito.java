c1973d17f27aeac94201f1e7c1de933f3fd5747a
ramiro galvan <ramiro_coronel333@hotmail.com>
2025-08-12 13:17:32 -0300

    agregando clase circuito

--- src/main/java/org/rami/Circuito.java
+++ src/main/java/org/rami/Circuito.java
@@ -1,4 +1,21 @@
 package org.rami;
 
-public class Circuito {
+public class Circuito implements Evaluable {
+    private int tiempo; // segundos
+    private int numFaltas;
+
+    public Circuito(int tiempo, int numFaltas) {
+        this.tiempo = tiempo;
+        this.numFaltas = numFaltas;
+    }
+
+    public int getTiempo() {
+        return tiempo;
+    }
+
+    @Override
+    public boolean estaAprobado() {
+        return numFaltas <= 2;
+    }
 }
+
