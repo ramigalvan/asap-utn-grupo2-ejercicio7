bfd2a0cea91e308307133ca38606ff75fc31749b
ramiro galvan <ramiro_coronel333@hotmail.com>
2025-08-12 13:15:06 -0300

    agregando clase persona

--- src/main/java/org/rami/Persona.java
+++ src/main/java/org/rami/Persona.java
@@ -1,4 +1,14 @@
 package org.rami;
 
 public class Persona {
+    private String dni;
+    private String nombre;
+    private String apellido;
+
+    public Persona(String dni, String nombre, String apellido) {
+        this.dni = dni;
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+
 }
