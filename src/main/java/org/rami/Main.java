8a2b4d07b3c12a9baea70246a4629e05248628a6
ramiro galvan <ramiro_coronel333@hotmail.com>
2025-08-12 13:15:57 -0300

    agregando clase main

--- src/main/java/org/rami/Main.java
+++ src/main/java/org/rami/Main.java
@@ -1,17 +1,44 @@
 package org.rami;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Sede sede = new Sede("Sede Central");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        ExamenAuto examenAuto01 = new ExamenAuto(
+                "2022-04-10",
+                new Persona("11111111", "Laura", "Martinez"),
+                new Auto("IHW900", "Fiat", 1400, 5),
+                new Circuito(10, 2)
+        );
+
+        ExamenAuto examenAuto02 = new ExamenAuto(
+                "2022-04-10",
+                new Persona("22222222", "Marcelo", "Perez"),
+                new Auto("LFP182", "Renault", 1400, 5),
+                new Circuito(5, 2)
+        );
+
+        ExamenAuto examenAuto03 = new ExamenAuto(
+                "2022-04-10",
+                new Persona("33333333", "Victoria", "Gomez"),
+                new Auto("CHC078", "Renault", 1400, 5),
+                new Circuito(100, 1)
+        );
+
+        ExamenMoto examenMoto01 = new ExamenMoto(
+                "2022-04-10",
+                new Persona("44444444", "Damian", "Lopez"),
+                new Moto("PAS911", "Kawasaki", 250, true)
+        );
+
+        examenMoto01.agregarCircuito(new Circuito(120, 1));
+        examenMoto01.agregarCircuito(new Circuito(110, 0));
+
+        sede.agregarExamen(examenAuto01);
+        sede.agregarExamen(examenAuto02);
+        sede.agregarExamen(examenAuto03);
+        sede.agregarExamen(examenMoto01);
+
+        System.out.println(sede.obtenerInforme());
     }
 }
